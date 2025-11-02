f47d6e7284631f81d034e5696c784fa2f49dff91
50275 <68676339+50275@users.noreply.github.com>
2025-11-01 20:24:44 -0700

    works by Vars.ui.planet.playSelected()

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,20 @@
+var planetDialogO; 
+var planetDialogC; 
+var canSelectM; 
+var time = 0; 
+Events.on(ClientLoadEvent, event => {
+    // this shouldn't happen
+    if (planetDialogO) return; 
+    planetDialogO = Vars.ui.planet; 
+    planetDialogC = planetDialogO.getClass(); 
+    canSelectM = planetDialogC.getDeclaredMethod("canSelect", [Sector]); 
+    canSelectM.setAccessible(true); 
+}); 
+
 function switchSector(target){
+    // You have to be the host or this mod just disconnects you. 
+    if(Vars.net.client()) return "You aren't the host."; 
+
     // This function is only meaningful if you are in the campaign. 
     if(!Vars || !Vars.state || !Vars.state.sector || !Vars.state.sector.planet) return "You aren't in the campaign."; 
 
@@ -16,18 +32,16 @@ function switchSector(target){
 
     // reflection
     try{
-        let planetDialogO = Vars.ui.planet; 
-        let planetDialogC = planetDialogO.getClass(); 
-        
         // The sector has to be accessible. 
-        let canSelectM = planetDialogC.getDeclaredMethod("canSelect", [Sector]); 
-        canSelectM.setAccessible(true); 
         if(!canSelectM.invoke(planetDialogO, sector)) return "You can't select this sector: " + String(target); 
 
         // TODO does this work? 
         let playSelectedM = planetDialogC.getDeclaredMethod("playSelected", null); 
         playSelectedM.setAccessible(true); 
-        planetDialogO.selected = sector; 
+
+        // planetDialogO.selected = sector; 
+        planetDialogO.selectSector(sector); 
+
         playSelectedM.invoke(planetDialogO, null); 
         
         return "HOORAY"; 
@@ -35,6 +49,9 @@ function switchSector(target){
     catch(error) {
         return String(error); 
     }
+}
+function sayAccessible(){
+    
 }
 const commands = [
     {
@@ -49,7 +66,7 @@ const commands = [
     }, 
     {
         word: "#near",
-        action: u => print(Vars.state.sectors.near())
+        action: () => print(Vars.state.sector.near())
     }
 ]; 
 Events.on(PlayerChatEvent, (input) => {
@@ -64,9 +81,18 @@ Events.on(PlayerChatEvent, (input) => {
         print("command null");
         return; 
     }
+
     else{
         // @Nullable
         let parameters = temp.slice(1); 
-        print(command.action(parameters));
+        try{
+            print(command.action(parameters));
+        } catch(error){
+            print(error); 
+        }
     }
 });
+
+/*Okay. The better question is this: LaunchLoadoutDialog and the logic contained within it is the only way to properly initiate a launch. Specifically, it's the only way to choose the launch loadout, check if the launch loadout is valid, check if you have the resources to do so... it's really the backbone for all the logic in addition to the UI itself. However, LaunchLoadoutDialog is designed with the assumption that you are the host. Use it to launch on a server and it will send you to your own singleplayer game in your campaign. This is because whatever `Runnable confirm` is passed to LaunchLoadoutDialog.show() is clearly targeting the client's sector, as 
+
+That's where I come in. It's clear that  */
